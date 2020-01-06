ecb003015bccb404a75d13b7fb629b3712338cb2
dandan-drori <55210781+dandan-drori@users.noreply.github.com>
2020-01-06 23:00:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+const gradients = [
+  'linear-gradient(to top right, #f12711, #f5af19)',
+  'linear-gradient(to top right, #2193b0, #6dd5ed)',
+  'linear-gradient(to top right, #7f00ff, #e100ff)'
+];
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach(entry => {
+    const id = entry.target.id;
+    const activeAnchor = document.querySelector(`[data-page=${id}]`);
+    const gradientIndex = entry.target.getAttribute('data-index');
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left
+    };
+    if (entry.isIntersecting) {
+      bubble.style.visibility = 'visible';
+      bubble.style.setProperty('left', `${directions.left}px`);
+      bubble.style.setProperty('top', `${directions.top}px`);
+      bubble.style.setProperty('width', `${directions.width}px`);
+      bubble.style.setProperty('height', `${directions.height}px`);
+      bubble.style.background = gradients[gradientIndex];
+    }
+  });
+}
+
+sections.forEach(section => {
+  observer.observe(section);
+});
